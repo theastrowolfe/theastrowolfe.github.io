da3284b21acb34448d98fe64237ed65d6341269c
Joshua Wolfe <developer.wolfe@gmail.com>
2021-04-06 20:23:41 -0500

    add usage to new.sh

--- new.sh
+++ new.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 # Usage: hugo new blog hacking-the-universe
 # Usage: hugo new gist shred.py
+if [ "$#" -ne 2 ]; then
+    echo "Usage: new.sh [blog/gist] [TITLE]"
+    exit 1
+fi
+
 hugo new $1/$2/index.md
