5ae4531c5c242de305b8d08d74a3e6d43edb3917
Joshua Wolfe <developer.wolfe@gmail.com>
2022-01-11 16:07:15 -0600

    fix deployment script

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,10 @@
 
 # If a command fails then the deploy stops
 set -e
+# Updating master
+git checkout master
+git pull
+git checkout dev
 #
 printf "\033[0;32mDeploying updates to GitHub...\033[0m\n"
 # Build the project.
